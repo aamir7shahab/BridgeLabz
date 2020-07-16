d2b564279e8f8b05ca9d27ab97a48789efe13c6a
aamir7shahab <aamir.shahab007@gmail.com>
2020-07-16 12:53:29 +0530

    Monthly wage functionality added.

--- JavaPrograms/day19EmployeeWage.java
+++ JavaPrograms/day19EmployeeWage.java
@@ -3,26 +3,32 @@ class day19EmployeeWage {
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 2
 	
 	public static void main(String[] args) {
 		System.out.println("-----------------Welcome To Employee Wage Computation-----------------");
 		// Variables
 		int empHrs=0;
 		int empWage=0;
+		int totalWage=0;
 
 		// Computation
-		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-		switch (empCheck) {
-			case IS_PART_TIME:
-				empHrs=4;
-				break;
-			case IS_FULL_TIME:
-				empHrs=8;
-				break;
-			default:
-				empHrs=0;
+		for (int day=0; day < NUM_OF_WORKING_DAYS ; day++ ) {
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+				case IS_PART_TIME:
+					empHrs=4;
+					break;
+				case IS_FULL_TIME:
+					empHrs=8;
+					break;
+				default:
+					empHrs=0;
+			}
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			totalWage += empWage;
+			System.out.println("Emp Wage: "+ empWage);
 		}
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage: "+ empWage);
+		System.out.println("Total Wage: "+ totalWage);
 	}
 }
