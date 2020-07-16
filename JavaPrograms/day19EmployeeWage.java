205f4bc706016115c8abbe47131249053bfeaa95
aamir7shahab <aamir.shahab007@gmail.com>
2020-07-16 12:25:03 +0530

    Added welcome note.

--- JavaPrograms/day19EmployeeWage.java
+++ JavaPrograms/day19EmployeeWage.java
@@ -1,13 +1,5 @@
 class day19EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("-----------------Welcome To Employee Wage Computation-----------------");
-		// Constant
-		int IS_FULL_TIME = 1;
-		// Computation
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == IS_FULL_TIME)
-			System.out.println("Employee is Present.");
-		else
-			System.out.println("Employee is Absent.");
 	}
 }
